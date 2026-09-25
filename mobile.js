a14fd34effedd58b2857b2cb8f3d60d273e0176c
INTJ <64084333+leefoxai@users.noreply.github.com>
2026-09-26 03:18:20 +0900

    chore: refresh mobile heading cache

--- mobile.js
+++ mobile.js
@@ -1,6 +1,6 @@
 (() => {
   'use strict';
-  const VERSION = '3.3.2-mobile-r5';
+  const VERSION = '3.3.2-mobile-r6';
   function load(src) {
     return new Promise((resolve, reject) => {
       const script = document.createElement('script');
